dc1a5d44821a8b23a02e842071eefaf8393055b6
Samuel Khongthaw <skthaw1@gmail.com>
2022-07-31 13:39:28 +0530

    minor fixes

--- scripts/routers/view-warranty.js
+++ scripts/routers/view-warranty.js
@@ -3,16 +3,20 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   var data = [];
+  var count = 0;
   try {
     const walletAddress = req.query.walletAddress;
     const url = `https://eth-goerli.alchemyapi.io/nft/v2/demo/getNFTs/?owner=${walletAddress}&contractAddresses=${process.env.CONTRACT_ADDRESS}`;
     let response = await fetch(url);
     response = await response.json();
+    if (response.ownedNfts.length === 0) {
+      res.send([]);
+    }
     response.ownedNfts.forEach(async (nft) => {
       let metadataUrl = nft.tokenUri.raw;
-      console.log(metadataUrl);
       try {
         let metadataResponse = await fetch(`${metadataUrl}`);
+        count++;
         let metadata = await metadataResponse.json();
         if (metadata?.record != undefined) {
           let warrantyInfo = {
@@ -22,9 +26,18 @@ router.get("/", async (req, res) => {
             expirationDate: metadata.record.expirationDate,
           };
           data.push(warrantyInfo);
+          if (response.ownedNfts.length <= count) {
+            res.status(200).send(data);
+          }
+          console.log("Count: " + count);
           console.log(data);
         }
-      } catch (e) {}
+      } catch (e) {
+        count++;
+        if (response.ownedNfts.length <= count) {
+          res.status(200).send(data);
+        }
+      }
     });
   } catch (e) {
     res.status(500).send({
