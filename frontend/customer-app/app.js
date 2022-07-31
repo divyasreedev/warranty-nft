22ed93ece0a46692a029fb581be78e388f518b7c
Samuel Khongthaw <skthaw1@gmail.com>
2022-07-31 21:22:55 +0530

    added customer frontend

--- frontend/customer-app/app.js
+++ frontend/customer-app/app.js
@@ -1,36 +1,48 @@
-const nfts = document.querySelector(".productHolder");
+const body = document.querySelector(".body");
 
-function createCard([productId, warrantyDetails, issueDate, expirationDate]){
-    let code = `
+function createCard([productId, warrantyDetails, issueDate, expirationDate]) {
+  let code = `
         <div class="card">
             <div class="cardText">
-                <h2 class="productId">${productId}</h2>
-                <h4 class="warrantyDetails">${warrantyDetails}</h4>
-                <p class="issueDate">${issueDate}</p>
-                <p class="expirationDate">${expirationDate}</p>
+                <h2 class="productId">Product Id: ${productId}</h2>
+                <h4 class="warrantyDetails">Warranty Details: ${warrantyDetails}</h4>
+                <p class="issueDate">Issue Date: ${issueDate}</p>
+                <p class="expirationDate">Expiry Date: ${expirationDate}</p>
             </div>
         </div>
     `;
-    nfts.innerHTML += code;
+  const nfts = document.querySelector(".productHolder");
+  nfts.innerHTML += code;
 }
 
-let json = [
-    {
-     productId: "123",
-     warrantyDetails: "hello world",
-     issueDate: "2022-07-28",
-     expirationDate: "2022-07-29"
-     },
- 
-    {
-     productId: "232",
-     warrantyDetails: "hi world",
-     issueDate: "2022-07-28",
-     expirationDate: "2022-07-29"
-     },
-     
- ]; 
-
-for(i = 0; i < json.length; i++){
-    createCard([json[i].productId, json[i].warrantyDetails, json[i].issueDate, json[i].expirationDate]);
+async function getData() {
+  const walletAddress = prompt("Please enter your wallet address: ");
+  let response = await fetch(
+    `http://localhost:3000/viewWarrantyTokens/?walletAddress=${walletAddress}`
+  );
+  let data = await response.json();
+  body.innerHTML =
+    '<div class="productHolder"><h1>You have the following products:</h1></div>';
+  if (data.length === 0) {
+    body.innerHTML = '<p class="loader-text">No Warranties found</p>';
+  }
+  for (i = 0; i < data.length; i++) {
+    createCard([
+      data[i].productId,
+      urlify(data[i].warrantyDetails),
+      data[i].issueDate.split(" ")[0],
+      data[i].expirationDate.split(" ")[0],
+    ]);
+  }
 }
+
+function urlify(text) {
+  var urlRegex = /(https?:\/\/[^\s]+)/g;
+  return text.replace(urlRegex, function (url) {
+    return '<a href="' + url + '">' + url + "</a>";
+  });
+}
+
+body.innerHTML =
+  '<center class="loader"></center><p class="loader-text">Loading your data...</p>';
+getData();
